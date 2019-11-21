b54a8b339713059ee78e05aefecfd78af9f70338
arkamis <miguelantonio_rodriguez@ucol.mx>
2019-11-21 17:11:17 -0600

    Intento funcional 01

--- test.py
+++ test.py
@@ -4,15 +4,19 @@ from datetime import datetime as dt
 import time
 from morseDict import MORSE_CODE_DICT
 from grove.factory import Factory
-import threading
 # connect to pin 5 (slot D5)
 PIN = 5
 button = GroveButton(PIN)
 led = GroveLed(16)
 
-baseSecTimer = input("Please select the timing beat")
+#setting variables for out login process
+baseSecTimer = input("Please select the timing beat in secs: ")
+Tolerance =  baseSecTimer / 2.0
 print("Timing base selected, now its {} secs".format(baseSecTimer))
 
+pinCode= []
+codeString = ''
+onProcess = False
 def on_Press(t):
 	"""
 	funcion para imprimir current status
@@ -20,11 +24,22 @@ def on_Press(t):
 	print("SI funciona el boton, imprimire t:", t)
 
 def on_Release(t):
+	global onProcess
 	"""
 	funcion como controlador, iniciaria nuestras funciones 
 	y hara algo con el valor obtenido del time
 	"""
 	print("Liberado: ", round(t,2))
+	if not onProcess and t > 6:
+		startEngine()
+		blink()
+		get_Ping_Code(t)
+	elif onProcess and len(pinCode) == 4 and t > 6:
+		print("Apagando!")
+	elif onProcess:
+		get_Ping_Code(t)
+	else:
+		print("NO entre!")
 
 def get_time_onseconds(time1, time2):
 	deltaTime = time2 - time1
@@ -39,41 +54,42 @@ def startEngine():
 	print("Starting Morse Detector")
 	time.sleep(2)
 	print("Please press button to start submit your 4-digit pin")
- 
-	pinNumber = get_Ping_Code()
-	print(pinNumber)
 
-def get_Ping_Code(pinCode = []):
+def get_Ping_Code(t):
+	global codeString
 	while len(pinCode) < 4:
 		print("Leyendo la posicion {} del pin de 4 digitos".format(len(pinCode)))
-  
-		#Itera hasta tener una string de 5 digitos 
-		codeString = pin_String()
-		#string valida, sino volvemos a iterar
-		if codeString in MORSE_CODE_DICT.values():
-			pinConvertido = MORSE_CODE_DICT.keys()[MORSE_CODE_DICT.values().index(codeString)]
-			pinCode.append(pinConvertido)
-		else:
-			print("Pin no valido, repetir digitos!")
-		get_Ping_Code(pinCode)
-	return pinCode
-        
-def pin_String(codeString = ""):    
-	while len(codeString) < 5:
-		button.on_press = press
-		button.on_release = release
 
-		elapse = press-release
-		if elapse.total_seconds() (2/baseSecTimer):
+		pin_String(t)
+		if len(codeString) == 5:
+			#string valida, sino volvemos a iterar
+			if codeString in MORSE_CODE_DICT.values():
+				pinConvertido = MORSE_CODE_DICT.keys()[MORSE_CODE_DICT.values().index(codeString)]
+				pinCode.append(pinConvertido)
+				codeString = ''
+			else:
+				print("Pin no valido, repetir digitos!")
+				codeString = ''
+		return True
+	return pinCode
+
+def pin_String(t):
+	global codeString
+	while len(codeString) < 5:
+		
+		if t > (2/baseSecTimer):
 			codeString += '-'
 			print("Tu string: {},  ahora lleva una - mas".format(codeString))
-			pin_String(codeString)
-		elif elapse.total_seconds() < (1/baseSecTimer):
+		elif t < (1/baseSecTimer):
 			codeString += '.'
 			print("Tu string: {},  ahora lleva una . mas".format(codeString))
-			pin_String(codeString)
 		else:
 			print("Nose que pusiste")
-			pin_String(codeString)
 	return codeString 
 
+def blink():
+	while True:
+		led.on()
+		time.sleep(baseSecTimer / 2.0)
+		led.off()
+		time.sleep(baseSecTimer)
