f87b4dec86bce82651215af7028376502f7d9578
arkamis <miguelantonio_rodriguez@ucol.mx>
2019-11-22 00:23:53 -0600

    Mejorando script, agregando metodos para pin completo

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+import time
 from grove.grove_button import GroveButton
 from grove.grove_led import GroveLed
-from datetime import datetime as dt
-import time
-from morseDict import MORSE_CODE_DICT
 from grove.factory import Factory
+from datetime import datetime as dt
+from morseDict import MORSE_CODE_DICT
+
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
+
 # connect to pin 5 (slot D5)
 PIN = 5
 button = GroveButton(PIN)
@@ -32,11 +37,13 @@ def get_Ping_Code(t):
 		print("Leyendo la posicion {} del pin de 4 digitos".format(len(pinCode)))
 
 		pin_String(t)
+		##validate after adding user signal (dot or dash)
 		if len(codeString) == 5:
 			#string valida, sino volvemos a iterar
 			if codeString in MORSE_CODE_DICT.values():
 				pinConvertido = MORSE_CODE_DICT.keys()[MORSE_CODE_DICT.values().index(codeString)]
 				pinCode.append(pinConvertido)
+				print "Se finalizo el numero! MORSE STRING: {}, NUMERO CONVERTIDO: {}".format(codeString, pinConvertido)
 				codeString = ''
 			else:
 				print("Pin no valido, repetir digitos!")
@@ -61,7 +68,29 @@ def blink():
 	time.sleep(baseSecTimer / 2.0)
 	led.off()
 	time.sleep(baseSecTimer)
-  
+
+
+### Seccion de REQUESt COAP
+def check_pincode():
+	global pinCode
+	"""
+	Funcion para enviar peticion coap y hacer algo con la respuesta!
+	Objetivo: Simular la peticion COAP Al servidor e imprime resultado!
+	"""
+	pinCodeMutation = list(pinCode)
+	pin_code_toString = "".join(pinCodeMutation)
+	if len(pin_code_toString) == 4:
+		print "Pin code validated!"
+		match = authenticate_method(pin_code_toString)
+		if match:
+			print "Welcome: {}, your pin code is:{}".format(match["name"], match["pin_number"])
+		else:
+			print "Authentication Failed, No se ha encontrado usuario con ese PIN"
+			pinCode = []
+	else:
+		print "Invalid Pin!, try againg"
+		pinCode = []
+##controllers!!
 def on_Press(t):
     	"""
 	funcion para imprimir current status
@@ -99,3 +128,21 @@ button.on_release = on_Release
 while True:
     time.sleep(1)
 	
+ 
+def authenticate_method(pin):
+	"""
+	Esta funcion simula la el controlador de la peticion COAP
+	Nos regresa un dict, vacio si no encontro usuario con ese pin
+	Objetivo: Obtener un matching con el PIN
+	"""
+	users = (
+		{"name": "MIke", "pin_number": "4004"},
+		{"name": "Maria", "pin_number":"5045"},
+		{"name": "Moctzyma", "pin_number":"1029"},
+		{"name": "Itzel", "pin_number":"3202"}
+	)
+	match = {}
+	for user in users:
+		if pin == user["pin_number"]:
+			match = user
+	return match
